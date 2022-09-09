9b02663a53b3c797fbf5f0ca8ee4f2fea02d23ef
Joseph Chinonso <iamjosephchinonso35@gmail.com>
2022-09-09 16:22:57 -0700

    t12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,18 +17,26 @@ int main(void)
 			if (p > i)
 			{
 				putchar((i / 10) + '0');
+
 				putchar((i % 10) + '0');
+
 				putchar(' ');
+
 				putchar((p / 10) + '0');
+
 				putchar((p % 10) + '0');
+
 				if (i != 98)
+
 				{
 					putchar(',');
+
 					putchar(' ');
 				}
 			}
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
