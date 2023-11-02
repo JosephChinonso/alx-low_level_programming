e8b347378367e0837400b433a3d311e786f5cfb4
Josephchinonso <iamjosephchinonso35@gmail.com>
2023-11-02 07:05:59 -0700

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -47,35 +47,3 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
    return (s);
 }
 
-
-== == == == = 2-calloc.c ====
-
-
-#include "main.h"
-
-
-/**
-* _calloc - allocates memory for an array of @nmemb elements of
-* @size bytes each and returns a pointer to the allocated memory.
-*
-* @nmemb: allocate memory for array
-* @size: allocate element of size bytes
-*
-* Return: pointer to the allocated memory.
-*/
-void * _calloc(unsigned int nmemb, unsigned int size)
-{
-   char *a;
-   unsigned int b;
-
-
-   if (nmemb == 0 || size == 0)
-       return (NULL);
-   a = malloc(nmemb * size);
-   if (a == NULL)
-       return (NULL);
-   for (b = 0; b < (nmemb * size); b++)
-       a[b] = 0;
-   return (a);
-}
-
