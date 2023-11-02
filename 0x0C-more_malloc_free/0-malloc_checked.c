6fc33443cc62d0ee498a256cfb0f3a405637140d
Josephchinonso <iamjosephchinonso35@gmail.com>
2023-11-02 05:47:37 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,51 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
-char *string_nconcat(char *s1, char *s2, unsigned int n) {
-    unsigned int len1 = 0, len2 = 0;
-    char *result;
-    
-    if (s1 == NULL) {
-        s1 = "";
-    }
-    if (s2 == NULL) {
-        s2 = "";
-    }
 
-    len1 = strlen(s1);
-    len2 = strlen(s2);
+/**
+* malloc_checked - cause normal process termination with a status value of 98
+*
+* @b: allocated memory
+*
+* Return: a pointer to the allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+   void *i;
 
-    if (n >= len2) {
-        n = len2;
-    }
 
-    result = (char *)malloc(sizeof(char) * (len1 + n + 1));
-
-    if (result == NULL) {
-        return NULL;
-    }
-
-    strcpy(result, s1);
-    strncat(result, s2, n);
-
-    return result;
-}
-
-int main() {
-    char *s1 = "Hello, ";
-    char *s2 = "world!";
-    unsigned int n = 5;
-
-    char *result = string_nconcat(s1, s2, n);
-
-    if (result != NULL) {
-        printf("Concatenated string: %s\n", result);
-        free(result);
-    } else {
-        printf("Failed to allocate memory for the concatenated string.\n");
-    }
-
-    return 0;
+   i = malloc(b);
+   if (i == NULL)
+       exit(98);
+   return (i);
 }
 
