d0dd11e5762a3457018bc274791aa10b0f8a361f
Josephchinonso <iamjosephchinonso35@gmail.com>
2024-01-15 18:41:18 -0800

    update

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-gcc -c *.c -fpic && gcc *.o -shared -o liball.so
+
+# Compile all .c files into object files
+gcc -c *.c
+
+# Create a dynamic library named liball.so from the object files
+gcc -shared -o liball.so *.o
+
+# Clean up the temporary object files
+rm *.o
+
